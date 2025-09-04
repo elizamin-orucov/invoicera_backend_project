942dd8f938d14e0f069aa5cabd07a01dd379f230
elizamin-orucov <elizaminoruc599@gmail.com>
2025-09-04 14:27:38 +0400

    gateway cors access

---
+++ business-data-service/src/main/java/com/business_data_service/dtos/invoice/InvoiceAutomaticListDto.java
@@ -0,0 +1,11 @@
+package com.business_data_service.dtos.invoice;
+
+import com.business_data_service.base.BaseIdDto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class InvoiceAutomaticListDto extends BaseIdDto {
+//    private String
+}
